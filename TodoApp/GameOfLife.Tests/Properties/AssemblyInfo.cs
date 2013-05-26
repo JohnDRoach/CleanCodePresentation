27defdad4b54a0fe1305e39a9934c36c0c5edf70
John D Roach <roach.john@gmail.com>
2013-05-26 17:51:50 +1000

    Added a test project and 1 test

---
+++ TodoApp/GameOfLife.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AssemblyInfo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssemblyInfo")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("094dbdc6-ec9f-4502-a4f4-3e73709b766d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TodoApp/GameOfLife.Tests/TestFixture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using GameOfLife;
+
+namespace GameOfLife.Tests
+{
+    [TestFixture]
+    public class TestFixture1
+    {
+        [Test]
+        public void CellWithOneNeighbourDies()
+        {
+            bool[,] world = new bool[3, 3];
+            world[1, 1] = true;
+            world[0, 0] = true;
+
+            Assert.IsFalse(new Incubator(world).Evolve()[1,1]);
+        }
+    }
+}
