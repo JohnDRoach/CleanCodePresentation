7718156fe5de992a7c1aa2d048098af8f36b1d51
John D Roach <roach.john@gmail.com>
2013-05-29 21:14:31 +1000

    After some intense refactoring

--- TodoApp/GameOfLife.Tests/AliveCellTests.cs
+++ TodoApp/GameOfLife.Tests/AliveCellTests.cs
@@ -8,8 +8,16 @@ using GameOfLife;
 namespace GameOfLife.Tests
 {
     [TestFixture]
-    public class Tests
+    public class AliveCellTests : CellTestFixture
     {
+        protected override bool StateOfCell
+        {
+            get
+            {
+                return Alive;
+            }
+        }
+
         [Test]
         public void CellWithNoNeighboursDies()
         {
@@ -80,48 +88,6 @@ namespace GameOfLife.Tests
             GivenCellWithThisManyNeighbours(8);
 
             CellIsDead(AfterEvolving());
-        }
-
-        
-        private IList<Cell> neighbours = new List<Cell>()
-        {
-            new Cell(0,0),
-            new Cell(0,1),
-            new Cell(0,2),
-            new Cell(1,0),
-            new Cell(1,2),
-            new Cell(2,0),
-            new Cell(2,1),
-            new Cell(2,2)
-        };
-
-        private Cell cell = new Cell(1, 1);
-        private bool[,] world;
-
-        private void GivenCellWithThisManyNeighbours(int numberOfNeighbours)
-        {
-            world = new bool[3, 3];
-            world[cell.X, cell.Y] = true;
-
-            foreach(Cell neighbour in neighbours.Take(numberOfNeighbours))
-            {
-                world[neighbour.X, neighbour.Y] = true;
-            }
-        }
-
-        private void CellIsAlive(bool value)
-        {
-            Assert.IsTrue(value);
-        }
-
-        private void CellIsDead(bool value)
-        {
-            Assert.IsFalse(value);
-        }
-
-        private bool AfterEvolving()
-        {
-            return new Incubator(world).Evolve()[cell.X, cell.Y];
         }
     }
 }
---
+++ TodoApp/GameOfLife.Tests/CellTestFixture.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace GameOfLife.Tests
+{
+    public abstract class CellTestFixture
+    {
+        protected const bool Alive = true;
+        protected const bool Dead = false;
+        protected abstract bool StateOfCell { get; }
+
+        protected IList<Cell> neighbours = new List<Cell>()
+        {
+            new Cell(0,0),
+            new Cell(0,1),
+            new Cell(0,2),
+            new Cell(1,0),
+            new Cell(1,2),
+            new Cell(2,0),
+            new Cell(2,1),
+            new Cell(2,2)
+        };
+
+        protected Cell cell = new Cell(1, 1);
+        protected bool[,] world;
+
+        protected void GivenCellWithThisManyNeighbours(int numberOfNeighbours)
+        {
+            world = new bool[3, 3];
+            world[cell.X, cell.Y] = StateOfCell;
+
+            foreach (Cell neighbour in neighbours.Take(numberOfNeighbours))
+            {
+                world[neighbour.X, neighbour.Y] = true;
+            }
+        }
+
+        protected void CellIsAlive(bool value)
+        {
+            Assert.IsTrue(value);
+        }
+
+        protected void CellIsDead(bool value)
+        {
+            Assert.IsFalse(value);
+        }
+
+        protected bool AfterEvolving()
+        {
+            return new Incubator(world).Evolve()[cell.X, cell.Y];
+        }
+    }
+}
