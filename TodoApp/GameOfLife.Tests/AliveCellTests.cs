f052c366af8cf4f0d8d9831c41f9635fddc6b2d0
John D Roach <roach.john@gmail.com>
2013-06-03 22:10:27 +1000

    Demo 4
    All the tests now.

--- TodoApp/GameOfLife.Tests/AliveCellTests.cs
+++ TodoApp/GameOfLife.Tests/AliveCellTests.cs
@@ -26,6 +26,62 @@ namespace GameOfLife.Tests
             CellIsDead(AfterEvolving());
         }
 
+        [Test]
+        public void CellWithTwoNeighboursSurvives()
+        {
+            GivenCellWithThisManyNeighbours(2);
+
+            CellIsAlive(AfterEvolving());
+        }
+
+        [Test]
+        public void CellWithThreeNeighboursSurvives()
+        {
+            GivenCellWithThisManyNeighbours(3);
+
+            CellIsAlive(AfterEvolving());
+        }
+
+        [Test]
+        public void CellWithFourNeighboursDies()
+        {
+            GivenCellWithThisManyNeighbours(4);
+
+            CellIsDead(AfterEvolving());
+        }
+
+        [Test]
+        public void CellWithFiveNeighboursDies()
+        {
+            GivenCellWithThisManyNeighbours(5);
+
+            CellIsDead(AfterEvolving());
+        }
+
+        [Test]
+        public void CellWithSixNeighboursDies()
+        {
+            GivenCellWithThisManyNeighbours(6);
+
+            CellIsDead(AfterEvolving());
+        }
+
+        [Test]
+        public void CellWithSevenNeighboursDies()
+        {
+            GivenCellWithThisManyNeighbours(7);
+
+            CellIsDead(AfterEvolving());
+        }
+
+        [Test]
+        public void CellWithEightNeighboursDies()
+        {
+            GivenCellWithThisManyNeighbours(8);
+
+            CellIsDead(AfterEvolving());
+        }
+
 
 
         private IList<Cell> neighbours = new List<Cell>()
