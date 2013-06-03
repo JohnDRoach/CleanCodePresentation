b8b8a0bc14f9cc2604f10481376c38eb8406cbdd
John D Roach <roach.john@gmail.com>
2013-06-03 21:57:57 +1000

    Demo 1
    Has the initial case test.

--- TodoApp/GameOfLife.Tests/AliveCellTests.cs
+++ TodoApp/GameOfLife.Tests/AliveCellTests.cs
@@ -8,86 +8,15 @@ using GameOfLife;
 namespace GameOfLife.Tests
 {
     [TestFixture]
-    public class AliveCellTests : CellTestFixture
+    public class AliveCellTests
     {
-        protected override bool StateOfCell
-        {
-            get
-            {
-                return Alive;
-            }
-        }
-
         [Test]
         public void CellWithNoNeighboursDies()
         {
-            GivenCellWithThisManyNeighbours(0);
+            bool[,] world = new bool[3, 3];
+            world[1, 1] = true;
 
-            CellIsDead(AfterEvolving());
-        }
-
-        [Test]
-        public void CellWithOneNeighbourDies()
-        {
-            GivenCellWithThisManyNeighbours(1);
-
-            CellIsDead(AfterEvolving());
-        }
-
-        [Test]
-        public void CellWithTwoNeighboursSurvives()
-        {
-            GivenCellWithThisManyNeighbours(2);
-
-            CellIsAlive(AfterEvolving());
-        }
-
-        [Test]
-        public void CellWithThreeNeighboursSurvives()
-        {
-            GivenCellWithThisManyNeighbours(3);
-
-            CellIsAlive(AfterEvolving());
-        }
-
-        [Test]
-        public void CellWithFourNeighboursDies()
-        {
-            GivenCellWithThisManyNeighbours(4);
-
-            CellIsDead(AfterEvolving());
-        }
-
-        [Test]
-        public void CellWithFiveNeighboursDies()
-        {
-            GivenCellWithThisManyNeighbours(5);
-
-            CellIsDead(AfterEvolving());
-        }
-
-        [Test]
-        public void CellWithSixNeighboursDies()
-        {
-            GivenCellWithThisManyNeighbours(6);
-
-            CellIsDead(AfterEvolving());
-        }
-
-        [Test]
-        public void CellWithSevenNeighboursDies()
-        {
-            GivenCellWithThisManyNeighbours(7);
-
-            CellIsDead(AfterEvolving());
-        }
-
-        [Test]
-        public void CellWithEightNeighboursDies()
-        {
-            GivenCellWithThisManyNeighbours(8);
-
-            CellIsDead(AfterEvolving());
+            Assert.IsFalse(new Incubator(world).Evolve()[1, 1]);
         }
     }
 }
